2eedcfd582ca321acab7063771c873f4d0a38c48
josemorenoo <josemoreno181818@gmail.com>
2023-03-15 02:33:27 -0700

    where are we again

--- config/bird_config.py
+++ config/bird_config.py
@@ -42,7 +42,7 @@ class CmcConfig:
     def __init__(self, sts_secrets: Optional[Dict[str, str]] = None):
         import os
 
-        print(os.getcwd())
+        print(os.getcwd(), PATHS["BIRD_CONFIG_FILE"])
         local_path = os.path.exists(PATHS["BIRD_CONFIG_FILE"])
         if local_path:
             print("CmcConfig loading secrets from local")
